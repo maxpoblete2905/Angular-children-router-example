90374be389955bc5665ccac0d6a83bb86135169e
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-04-14 23:48:56 -0400

    set deployd

--- src/app/shared/error404page/error404page.component.ts
+++ src/app/shared/error404page/error404page.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'shared-error404page',
-  template: '<p>error404page works!</p>',
+  template: `
+  <div class="error-404-container">
+    <h1>Error 404</h1>
+    <p>Oops! La página que estás buscando no pudo ser encontrada.</p>
+    <p>Pero no te preocupes, aquí hay algunas cosas que puedes hacer:</p>
+    <ul>
+      <li>Revisa la URL e inténtalo de nuevo.</li>
+      <li>Regresa a la página anterior.</li>
+      <li>Visita nuestra <a href="/">página de inicio</a> para explorar más.</li>
+    </ul>
+  </div>`,
   styleUrl: './error404page.component.css',
 })
 export class Error404pageComponent { }
